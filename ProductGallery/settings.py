a2e2ab650bf8eb6e362685cdcabf6d655c5dc39f
OderoCeasar <156299730+OderoCeasar@users.noreply.github.com>
2025-10-21 14:39:24 +0300

    setup mongodb

--- ProductGallery/settings.py
+++ ProductGallery/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from mongoengine import connect
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'products'
 ]
 
 MIDDLEWARE = [
@@ -79,6 +81,12 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+#connect mongodb
+connect(
+    db="product_gallery_db",
+    host="host='mongodb://localhost:27017/product_gallery_db",
+    )
 
 
 # Password validation
